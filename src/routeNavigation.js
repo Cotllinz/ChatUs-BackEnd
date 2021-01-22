e7e688e9dea7b0560a280eb2c1c440f688372397
Cotllinz <63383858+Cotllinz@users.noreply.github.com>
2021-01-23 06:24:39 +0800

    Update routeNavigation.js

--- src/routeNavigation.js
+++ src/routeNavigation.js
@@ -1,5 +1,8 @@
 const route = require('express').Router()
 const user = require('./routes/userRoutes')
-
+const friend = require('./routes/addFriendRoutes')
+const chat = require('./routes/chatRoutes')
 route.use('/user', user)
+route.use('/friend', friend)
+route.use('/chat', chat)
 module.exports = route
