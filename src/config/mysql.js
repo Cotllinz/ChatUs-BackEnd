f414d4f922a6c9d09d864dbb195e31950f5e060a
Cotllinz <63383858+Cotllinz@users.noreply.github.com>
2021-02-22 11:27:59 +0800

    Update mysql.js

--- src/config/mysql.js
+++ src/config/mysql.js
@@ -4,7 +4,7 @@ require('dotenv').config()
 const connection = mysql.createConnection({
   host: process.env.HOST,
   user: process.env.USER,
-  password: process.env.PASSWORD,
+  password: process.env.PASS,
   database: process.env.DATABASE
 })
 
